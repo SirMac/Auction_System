1f9da0e925a19e147ae37b25f42a02fcc3b8207e
SirMac <sirmc4u@gmail.com>
2024-10-20 16:45:26 -0400

    changed db to koyebdb

--- auctionApp/settings.py
+++ auctionApp/settings.py
@@ -83,6 +83,14 @@ WSGI_APPLICATION = 'auctionApp.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'koyebdb',
+        'USER': 'postgres',
+        'PASSWORD': '6QgXeZMYVm8E',
+        'HOST': 'ep-steep-water-a23rfiul.eu-central-1.pg.koyeb.app',
+        'OPTIONS': {'sslmode': 'require'},
+    },
+    'sqlite3': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
